458e0a2c9a21173ce5f6acb84d455608f947057f
Toni Valev <t.valev@7mojos.com>
2024-09-18 15:35:25 +0300

    Display recipes and edit mode

--- src/pages/Recipes/RecipesPage.tsx
+++ src/pages/Recipes/RecipesPage.tsx
@@ -8,7 +8,19 @@ import { Button, Form } from "react-bootstrap";
 export const RecipesPage: IRecipesPage = observer(() => {
   const {
     store: {
-      recipesStore: { editMode, setEditMode, addRecipe },
+      recipesStore: {
+        recipes,
+        isFormVisible,
+        editMode,
+        editedRecipe,
+        setIsFormVisible,
+        setEditMode,
+        addRecipe,
+        setEditedName,
+        setEditedDesc,
+        setEditedUrl,
+        submitRecipeForm,
+      },
     },
   } = useAppContext();
 
@@ -17,25 +29,49 @@ export const RecipesPage: IRecipesPage = observer(() => {
   const [url, setUrl] = useState("");
 
   return (
-    <div className="container w-100 d-flex">
+    <div className="container w-100 d-flex px-5">
       <div className="recipes w-50">
         <Button
           variant="success"
           className="add-button"
-          onClick={() => setEditMode(true)}
+          onClick={() => setIsFormVisible(true)}
         >
           New Recipe
         </Button>
+        <hr />
+        <div className="w-100 d-flex flex-column gap-1">
+          {recipes.map((rec) => {
+            return (
+              <div
+                className="recipe d-flex justify-content-between border border-black border-4 border-opacity-10 px-2 py-3"
+                onClick={() => setEditMode(true, rec)}
+              >
+                <div className="recipe__info">
+                  <h4>{rec.name}</h4>
+                  <span>{rec.description}</span>
+                </div>
+                <div className="recipe__image">
+                  <img src={rec.imageUrl} height={100} />
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
-      {editMode && (
-        <div className="form w-50">
+      {isFormVisible && (
+        <div className="form w-50 px-5">
           <Form>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
               <Form.Label>Name</Form.Label>
               <Form.Control
                 type="text"
                 placeholder="Recipe Name"
-                onChange={(e) => setName(e.target.value)}
+                onChange={(e) =>
+                  editMode
+                    ? setEditedName(e.target.value)
+                    : setName(e.target.value)
+                }
+                value={editMode && editedRecipe ? editedRecipe.name : name}
               />
             </Form.Group>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
@@ -43,9 +79,18 @@ export const RecipesPage: IRecipesPage = observer(() => {
               <Form.Control
                 type="text"
                 placeholder="Place your image url"
-                onChange={(e) => setUrl(e.target.value)}
+                onChange={(e) =>
+                  editMode
+                    ? setEditedUrl(e.target.value)
+                    : setUrl(e.target.value)
+                }
+                value={editMode && editedRecipe ? editedRecipe.imageUrl : url}
               />
             </Form.Group>
+            <img
+              src={editMode && editedRecipe ? editedRecipe.imageUrl : url}
+              height={200}
+            />
             <Form.Group
               className="mb-3"
               controlId="exampleForm.ControlTextarea1"
@@ -55,18 +100,34 @@ export const RecipesPage: IRecipesPage = observer(() => {
                 as="textarea"
                 rows={3}
                 placeholder="Describe your recipe"
-                onChange={(e) => setDesc(e.target.value)}
+                onChange={(e) =>
+                  editMode
+                    ? setEditedDesc(e.target.value)
+                    : setDesc(e.target.value)
+                }
+                value={
+                  editMode && editedRecipe ? editedRecipe.description : desc
+                }
               />
             </Form.Group>
           </Form>
           <div className="form__actions d-flex justify-content-between">
             <Button
               variant="success"
-              onClick={() => addRecipe(name, desc, url)}
+              onClick={() =>
+                submitRecipeForm({
+                  id: editMode && editedRecipe ? editedRecipe.id : "",
+                  name: editMode && editedRecipe ? editedRecipe.name : name,
+                  description:
+                    editMode && editedRecipe ? editedRecipe.description : desc,
+                  imageUrl:
+                    editMode && editedRecipe ? editedRecipe.imageUrl : url,
+                })
+              }
             >
               Save
             </Button>
-            <Button variant="danger" onClick={() => setEditMode(false)}>
+            <Button variant="danger" onClick={() => setIsFormVisible(false)}>
               Cancel
             </Button>
           </div>
--- src/stores/Recipes/store.ts
+++ src/stores/Recipes/store.ts
@@ -10,26 +10,85 @@ export class RecipesStore {
 
   @observable editMode: boolean = false;
 
+  @observable editedRecipe: IRecipe | undefined = undefined;
+
+  @observable isFormVisible: boolean = false;
+
   constructor() {
     
     makeObservable(this);
   }
 
   @action
-    setEditMode = (editMode: boolean) => {
-      this.editMode = editMode;
+    setIsFormVisible = (isFormVisible: boolean) => {
+      this.isFormVisible = isFormVisible;
     }
 
   @action
-  addRecipe = (name: string, description: string, imageUrl: string) => {
-    console.log("name", name)
-    console.log("description", description)
-    console.log("imageUrl", imageUrl)
-    this.recipes.push({
-      id: uniqueId(),
-      name,
-      description,
-      imageUrl
-    })
-  };
+    setEditedRecipe = (recipe: IRecipe) => {
+      this.editedRecipe = recipe;
+    }
+
+  @action
+    setEditMode = (editMode: boolean, recipe?: IRecipe) => {
+      this.editMode = editMode;
+      if (editMode === true) {
+        this.setIsFormVisible(true)
+        if (recipe) {
+          this.setEditedRecipe({...recipe})
+        }
+      }
+    }
+
+  @action
+    addRecipe = (name: string, description: string, imageUrl: string) => {
+      this.recipes.push({
+        id: uniqueId(),
+        name,
+        description,
+        imageUrl
+      })
+    };
+
+  @action
+    editRecipe = (recipe: IRecipe) => {
+      const foundRecipe = this.recipes.findIndex((r) => r.id === recipe.id);
+
+      if (!foundRecipe && foundRecipe < 0) {
+        return;
+      } else {
+        this.recipes[foundRecipe] = recipe
+      }
+    }
+
+  @action
+    setEditedName = (name: string) => {
+      if (this.editedRecipe)
+      this.editedRecipe.name = name;
+    }
+
+    @action
+    setEditedDesc = (desc: string) => {
+      if (this.editedRecipe)
+      this.editedRecipe.description = desc;
+    }
+
+    @action
+    setEditedUrl = (url: string) => {
+      if (this.editedRecipe)
+      this.editedRecipe.imageUrl = url;
+    }
+
+  submitRecipeForm = (recipe: IRecipe) => {
+    if (this.editMode) {
+      this.editRecipe(recipe);
+    } else {
+      this.addRecipe(
+        recipe.name,
+      recipe.description,
+        recipe.imageUrl
+      )
+    }
+    this.setIsFormVisible(false)
+  }
 }
